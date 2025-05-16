2943e662ea9a2bcdf5a53caddcc973c74bf4382e
Top dev <134450295+blockchaindev13579@users.noreply.github.com>
2025-05-16 21:54:07 +0900

    update

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:picturebook/Pages/Pay/PayPage.dart';
 import 'package:picturebook/Providers/book_provider.dart';
+import 'package:video_player/video_player.dart';
 
 class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
@@ -11,11 +12,37 @@ class HomePage extends ConsumerStatefulWidget {
 }
 
 class _HomePageState extends ConsumerState<HomePage> {
+  late VideoPlayerController _videoController;
+  late Future<void> _initializeVideoPlayerFuture;
+  bool _isVideoInitialized = false;
+
   @override
   void initState() {
     super.initState();
+    // Initialize video player
+    _videoController = VideoPlayerController.asset('assets/splashvideo.mp4');
+    
+    // networkUrl(
+    //   Uri.parse(
+    //     'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    //   ),
+    // );
+    _initializeVideoPlayerFuture = _videoController.initialize().then((_) {
+      setState(() {
+        _isVideoInitialized = true;
+      });
+      _videoController.play();
+      _videoController.setLooping(true);
+    });
+
     // Fetch books when the page loads
     Future.microtask(() => ref.read(booksProvider.notifier).fetchBooks());
+  }
+
+  @override
+  void dispose() {
+    _videoController.dispose();
+    super.dispose();
   }
 
   @override
@@ -39,7 +66,14 @@ class _HomePageState extends ConsumerState<HomePage> {
           ),
           Expanded(
             child: books.isEmpty
-                ? const Center(child: CircularProgressIndicator())
+                ? Center(
+                    child: _isVideoInitialized
+                        ? AspectRatio(
+                            aspectRatio: _videoController.value.aspectRatio,
+                            child: VideoPlayer(_videoController),
+                          )
+                        : const CircularProgressIndicator(),
+                  )
                 : ListView.builder(
                     itemCount: books.length,
                     itemBuilder: (context, index) {
