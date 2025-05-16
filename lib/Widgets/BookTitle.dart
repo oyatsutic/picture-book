bb714b2d41f8f075479428d6a7d10568105b4711
Top dev <134450295+blockchaindev13579@users.noreply.github.com>
2025-05-16 23:39:14 +0900

    update
    Co-Authored-By: Stella Ray <nextblock.dev@gmail.com>
    Co-Authored-By: ahita <185488843+ahita211@users.noreply.github.com>
    Co-Authored-By: YAMAMOTO <33847754+takuticyamamoto@users.noreply.github.com>

---
+++ lib/Widgets/BookTitle.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BookTitle extends StatefulWidget {
+  const BookTitle({super.key});
+
+  @override
+  State<BookTitle> createState() => _BookTitleState();
+}
+
+class _BookTitleState extends State<BookTitle> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
