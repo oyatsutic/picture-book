a0e3948503ae2f8c464b67c3f2246670655c037e
Top dev <134450295+blockchaindev13579@users.noreply.github.com>
2025-05-19 15:09:38 +0900

    update

--- lib/Screens/PDFScreen.dart
+++ lib/Screens/PDFScreen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:picturebook/Models/book.dart';
 // import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
+// import 'package:flutter_cached_pdfview/flutter_cached_pdfview.dart';
 import 'dart:io';
+import 'package:pdfx/pdfx.dart';
 
 class PdfScreen extends StatefulWidget {
   const PdfScreen({super.key, required this.book});
@@ -15,6 +19,19 @@ class _PdfScreenState extends State<PdfScreen> {
   String? _localPdfPath;
   bool _loading = true;
 
+  String pdfPath = "";
+  int totalPage = 1;
+  int currentPage = 1;
+  PdfControllerPinch pdfControllerPinch = PdfControllerPinch(
+    document: PdfDocument.openAsset('assets/books/pdf.pdf'),
+  );
+  PdfController pdfController = PdfController(
+    // document: PdfDocument.openFile(filePath)
+    document: PdfDocument.openAsset('assets/books/pdf.pdf'),
+  );
+
+  // PDFViewController? pdfViewController;
+
   @override
   void initState() {
     super.initState();
@@ -26,10 +43,11 @@ class _PdfScreenState extends State<PdfScreen> {
     final filePath = '${dir.path}/${widget.book.id}.pdf';
     final file = File(filePath);
     if (await file.exists()) {
-      
       setState(() {
         _localPdfPath = filePath;
         _loading = false;
+        pdfController =
+            PdfController(document: PdfDocument.openFile(_localPdfPath!));
       });
     } else {
       setState(() {
@@ -47,6 +65,8 @@ class _PdfScreenState extends State<PdfScreen> {
     if (_localPdfPath == null) {
       return const Center(child: Text('PDF not found.'));
     }
-    return SfPdfViewer.file(File(_localPdfPath!));
+    return PdfView(
+      controller: pdfController,
+    );
   }
 }
